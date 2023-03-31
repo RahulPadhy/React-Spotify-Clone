cd78f705821fae9d6c422d02bcd1d010c6fc8aad
Rahul Padhy <rahulpadhyrkp@gmail.com>
2023-03-31 18:54:04 +0530

    Spotify Clone Project Started

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Login from './Login';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './Player';
+import { useDataLayerValue } from './DataLayer';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  // Like a Temporary storage in react
+  const [token, setToken] = useState();
+  const [{ user }, dispatch] = useDataLayerValue();
+
+
+  // Run a code based on a given condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+
+    // convention for the one we are basically used for temporary storage
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        // console.log('Person >>', user);
+        dispatch({
+          type: 'SET_USER',
+          user: user
+        });
+      });
+    }
+
+    console.log('i have a token >>', token);
+  }, []); // Run once after the value inside square bracket changes can add as many values inside it
+
+  console.log("user >>", user);
+
   return (
+    // BEM convention for CSS
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        token ? (
+          <Player />
+        ) : (
+          <Login />
+        )
+      }
+
+
     </div>
   );
 }
---
+++ src/spotify.js
@@ -0,0 +1,29 @@
+// Authorize via spotify api 
+export const authEndpoint = "https://accounts.spotify.com/authorize/";
+
+// After authorization move dorectly to our own website
+const redirectUri = "http://localhost:3000/";
+
+const clientId = "66fa86cd584547db9a7a1db134d5c512";
+
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+];
+
+export const getTokenFromUrl = () => {
+    return window.location.hash
+        .substring(1)
+        .split('&')
+        .reduce((initial, item) => {
+            let parts = item.split('=');
+            initial[parts[0]] = decodeURIComponent(parts[1]);
+            return initial;
+        }, {});
+}
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
+
